2f832338568b648bf43e992c9dfb19c7b7d1b277
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:11:30 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MIKI GONZÁLEZ ';
+export let nameSongs = [
+	"CHICLES, CIGARRILLO, CARAMELOS", /*1*/
+	"VAMOS A TOCACHE", /*2*/
+	"EL MAR", /*3*/
+	"AKUNDUN", /*4*/
+	"TANTAS VECES", /*5*/
+	"PONTE TU VESTIDO", /*6*/
+	"HOJA VERDE", /*7*/
+	"FALSÍA", /*8*/
+	"CARDO O CENIZA", /*9*/
+	"ELLA ME DEJO", /*10*/
+	"PUEDES SER TÚ", /*11*/
+	"LA PEQUEÑA", /*12*/
+	"LOLA", /*13*/
+	"PELIGRO", /*14*/
+	"FANTASÍA DE TIJERAS", /*15*/
+	"BOSQUE DE NUBES", /*16*/
+	"RITTI PAMPA", /*17*/
+	"PITUMARCA", /*18*/
+	"MUNAQ", /*19*/
+	"LOCA JUVENTUD", /*20*/
+]
